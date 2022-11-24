c5ee46ee215b4bc13eda50ca69d976e1d30ade3c
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-11-25 01:11:18 +0600

    added users post api

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const port = process.env.PORT || 5000;
+
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +12,26 @@ app.use(express.json());
 app.get('/', (req, res) =>{
   res.send('rebooks server is running.');
 })
+
+const uri = `mongodb+srv://${process.env.REBOOKS_DB}:${process.env.DB_USER_PASS}@cluster0.jt8oxuk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  try{
+    const usersCollection = client.db('rebooksDb').collection('users');
+
+    app.post('/users', async(req, res) =>{
+      const user = req.body;
+      console.log(user);
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+  }
+  finally{
+
+  }
+}
+run().catch(err => console.log(err));
 
 app.listen(port, () => {
   console.log('rebooks server is running on port: ', port);
